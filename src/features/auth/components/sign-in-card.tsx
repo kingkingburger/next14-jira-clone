8a3739dd7fe6dff2e3077ae5bb6d7414dbb9b032
minho <dnjsalsgh123@gmail.com>
2024-11-25 20:32:29 +0900

    update: create custom separator that dotted-separator

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,17 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { DottedSeparator } from "@/components/dotted-separator";
+
+const SignInCard = () => {
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle>Welcom back!</CardTitle>
+      </CardHeader>
+      <div className="px-7 mb-2">
+        <DottedSeparator />
+      </div>
+    </Card>
+  );
+};
+
+export default SignInCard;
