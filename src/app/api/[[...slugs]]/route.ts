78adffc45a413ef7e2dca17cc1fa9484ec0db2cd
minho <dnjsalsgh123@gmail.com>
2024-12-01 07:35:29 +0900

    Update: using to params api used the elysia

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -14,6 +14,10 @@ const app = new Elysia({ prefix: "/api" })
   .get("/hello", () => {
     return { hello: "world2" }; // 잘못된 키 "wolrd"를 수정
   })
+  .get("/projectId/:projectId", ({ params }) => {
+    const { projectId } = params; // params에서 projectId 추출
+    return { project: projectId };
+  })
   .post("/", ({ body }) => body, {
     body: t.Object({
       name: t.String(),
