abe090909dbe95bdf2cae45add15d3849807d723
minho <dnjsalsgh123@gmail.com>
2024-12-03 23:12:32 +0900

    Update: implement the process that validation used zod-validator

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
+import { loginSchema } from "@/features/auth/schemas/login";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono().post("/login", (c) => {
+const app = new Hono().post("/login", zValidator("json", loginSchema), (c) => {
   return c.json({ success: "ok" });
 });
 
