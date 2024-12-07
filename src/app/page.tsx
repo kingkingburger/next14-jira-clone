e3b19a69d0187d5c3bf476463db6d82514b29b24
minho <dnjsalsgh123@gmail.com>
2024-12-08 07:41:50 +0900

    Fix: implement when logout success then remove the current user info in cookie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { useCurrent } from "@/features/auth/api/user-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
 
   useEffect(() => {
     if (!data && !isLoading) {
-      router.push("/sing-in");
+      router.push("/sign-in");
     }
   }, [data]);
 
-  return <div className="">Only visible to authorized users.</div>;
+  return (
+    <div className="">
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
