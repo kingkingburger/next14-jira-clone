64eb09d760e5f3320ab31e6b805d7cedc5d89d9b
minho <dnjsalsgh123@gmail.com>
2024-12-10 20:41:41 +0900

    Update: make the custom user button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import { useRouter } from "next/navigation";
 
 import { useCurrent } from "@/features/auth/api/user-current";
 import { useLogout } from "@/features/auth/api/use-logout";
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
+
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
@@ -19,8 +20,7 @@ export default function Home() {
 
   return (
     <div className="">
-      Only visible to authorized users.
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />{" "}
     </div>
   );
 }
---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useCurrent } from "@/features/auth/api/user-current";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Loader } from "lucide-react";
+
+export const UserButton = () => {
+  const { data: user, isLoading } = useCurrent();
+
+  if (isLoading) {
+    return (
+      <div className="size-10 rounded-full flex items-center justify-center bg-neutral-200 border border-neutral-300">
+        <Loader className="size-4 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  const { name, email } = user;
+  const avatarFallback = name
+    ? name.charAt(0).toUpperCase()
+    : (email.charAt(0).toUpperCase() ?? "U");
+
+  return (
+    <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
+      <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+        {avatarFallback}
+      </AvatarFallback>
+    </Avatar>
+  );
+};
