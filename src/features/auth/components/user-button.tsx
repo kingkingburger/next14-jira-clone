30170ad067b2add261c4e00debef7eb3983e9d5e
minho <dnjsalsgh123@gmail.com>
2024-12-12 19:50:51 +0900

    Update: positioning a Logout button in dropdown menu

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -2,14 +2,18 @@
 
 import { useCurrent } from "@/features/auth/api/user-current";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { useLogout } from "@/features/auth/api/use-logout";
 
 export const UserButton = () => {
+  const { mutate: logout } = useLogout();
   const { data: user, isLoading } = useCurrent();
 
   if (isLoading) {
@@ -45,12 +49,26 @@ export const UserButton = () => {
         sideOffset={10}
       >
         <div className="flex flex-col items-center gap-2 px-2.5 py-4">
-          <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
-            <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+          <Avatar className="size-[52px] hover:opacity-75 transition border border-neutral-300">
+            <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
               {avatarFallback}
             </AvatarFallback>
           </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">
+              {name || "User"}
+            </p>
+            <p className="text-xs  text-neutral-500">{email}</p>
+          </div>
         </div>
+        <DottedSeparator className="mb-1" />
+        <DropdownMenuItem
+          onClick={() => logout()}
+          className="h-10 flex item-center justify-center text-amber-700 font-medium cursor-pointer"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
