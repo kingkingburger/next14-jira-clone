6b8908190c059d2ef4a77c87d427c55967c55564
minho <dnjsalsgh123@gmail.com>
2024-12-22 06:31:52 +0900

    update: managing close effect when move to another route

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -4,10 +4,19 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { MenuIcon } from "lucide-react";
 import { Sidebar } from "@/components/sidebar";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export const MobileSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   return (
-    <Sheet modal={false}>
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild={true}>
         <Button variant="secondary" className="lg:hidden">
           <MenuIcon className="size-5 text-neutral-500" />
