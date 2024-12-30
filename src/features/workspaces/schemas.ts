cd3ad16356d05ae34293dda998302f24476eaa92
minho <dnjsalsgh123@gmail.com>
2024-12-30 20:23:10 +0900

    update: setting schema in workspace

--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createWorkspacesSchema = z.object({
   name: z.string().trim().min(1, "Required"),
-  imageUrl: z
+  image: z
     .union([
       z.instanceof(File),
       z.string().transform((value) => (value === "" ? undefined : value)),
