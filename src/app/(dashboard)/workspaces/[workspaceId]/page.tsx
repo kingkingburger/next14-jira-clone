7d1ea6026d828905f6c632981cb1e3a7782e1976
minho <dnjsalsgh123@gmail.com>
2025-01-06 19:51:42 +0900

    update: show workspace name when select workspace

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,5 @@
+const WorkspaceIdPage = () => {
+  return <div>workspaceId</div>;
+};
+
+export default WorkspaceIdPage;
--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { RiAddCircleFill } from "react-icons/ri";
+import { useRouter } from "next/navigation";
+
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
-import { RiAddCircleFill } from "react-icons/ri";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 import {
   Select,
@@ -13,7 +16,13 @@ import {
 } from "@/components/ui/select";
 
 export const WorkspaceSwitcher = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -21,7 +30,7 @@ export const WorkspaceSwitcher = () => {
         <p className="text-xs uppercase text-neutral-500">Workspace</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected" />
         </SelectTrigger>
