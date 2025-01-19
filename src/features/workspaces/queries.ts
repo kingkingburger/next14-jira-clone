28d693c257e12e82eac541772c4556177eb58ee1
minho <dnjsalsgh123@gmail.com>
2025-01-19 16:35:13 +0900

    refactoring: modify get sessionClient manage

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -13,17 +13,7 @@ import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = await cookies().get(AUTH_COKKIE);
-
-    if (!session) return { documents: [], total: 0 };
-
-    client.setSession(session.value);
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
