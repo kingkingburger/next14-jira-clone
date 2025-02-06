9ef7b2c7645d61962aa55ea010237e9b1b561554
minho <dnjsalsgh123@gmail.com>
2025-02-06 20:35:07 +0900

    update: getting workspace Name in workspace join component

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,4 +1,19 @@
-const WorkspaceIdJoinPage = () => {
-  return <div>workspace ID join page</div>;
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
+import { getWorkspaceInfo } from "@/features/workspaces/queries";
+
+interface WorkspaceIdJoinPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const WorkspaceIdJoinPage = async ({ params }: WorkspaceIdJoinPageProps) => {
+  const user = await getCurrent();
+  if (!user) return redirect("/sign-in");
+
+  const workspace = await getWorkspaceInfo({ workspaceId: params.workspaceId });
+
+  return <div>workspace ID : {JSON.stringify(workspace)}</div>;
 };
 export default WorkspaceIdJoinPage;
