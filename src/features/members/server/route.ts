139737a4bbff727fe0a217846a5e45da907adf16
minho <dnjsalsgh123@gmail.com>
2025-02-13 19:28:01 +0900

    update: create member update api

--- src/features/members/server/route.ts
+++ src/features/members/server/route.ts
@@ -83,7 +83,7 @@ const app = new Hono()
     }
 
     if (allMembersInWorkspace.total === 1) {
-      return c.json({ error: "Cannot delete the only member" });
+      return c.json({ error: "Cannot delete the only member" }, 400);
     }
 
     await databases.deleteDocument(DATABASE_ID, MEMBERS_ID, memberId);
@@ -94,7 +94,50 @@ const app = new Hono()
     "/:memberId",
     sessionMiddleware,
     zValidator("json", z.object({ role: z.nativeEnum(MemberRole) })),
-    async (c) => {},
+    async (c) => {
+      const { memberId } = c.req.param();
+      const { role } = c.req.valid("json");
+      const user = c.get("user");
+      const databases = c.get("databases");
+
+      const memberToUpdate = await databases.getDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        memberId,
+      );
+
+      const allMembersInWorkspace = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        [Query.equal("workspaceId", memberToUpdate.workspaceId)],
+      );
+
+      const member = await getMembers({
+        databases,
+        workspaceId: memberToUpdate.workspaceId,
+        userId: user.$id,
+      });
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      if (
+        member.id !== memberToUpdate.$id &&
+        member.role !== MemberRole.ADMIN
+      ) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      if (allMembersInWorkspace.total === 1) {
+        return c.json({ error: "Cannot downgrade the only member" }, 400);
+      }
+
+      await databases.updateDocument(DATABASE_ID, MEMBERS_ID, memberId, {
+        role,
+      });
+
+      return c.json({ data: { $id: memberToUpdate.$id } });
+    },
   );
 
 export default app;
