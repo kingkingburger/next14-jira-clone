8db98c8987a1f5f1fda9e63c2a1ce16d78df6cf4
minho <dnjsalsgh123@gmail.com>
2025-02-19 19:38:55 +0900

    update: initialize members component

--- src/features/workspaces/components/members-list.tsx
+++ src/features/workspaces/components/members-list.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { ArrowLeftIcon } from "lucide-react";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { DottedSeparator } from "@/components/dotted-separator";
+
 export const MembersList = () => {
-  return <div>Members list</div>;
+  const workspaceId = useWorkspaceId();
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
+        <Button asChild={true} variant="secondary" size="sm">
+          <Link href={`/workspaces/${workspaceId}`}>
+            <ArrowLeftIcon className="size-4 mr-2" />
+            Back
+          </Link>
+        </Button>
+        <CardTitle className="text-xl font-bold">Members list</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7"></CardContent>
+    </Card>
+  );
 };
