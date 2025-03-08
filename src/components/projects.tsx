c8313fc77f70ba248e9089d793b3af533aad383c
minho <dnjsalsgh123@gmail.com>
2025-03-08 09:23:11 +0900

    update: create useGetProject

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import { RiAddCircleFill } from "react-icons/ri";
+import { useGetProjects } from "@/features/project/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Projects = () => {
+  const projectId = null; // TODO: Use the useProject hook
+
+  const pathname = usePathname();
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetProjects({
+    workspaceId: workspaceId,
+  });
+
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -12,6 +24,12 @@ export const Projects = () => {
           className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
         />
       </div>
+      {data?.documents.map((project) => {
+        const href = `workspaces/${workspaceId}projects/${projectId}`;
+        const isActive = pathname === href;
+
+        return <Link href={href}></Link>;
+      })}
     </div>
   );
 };
