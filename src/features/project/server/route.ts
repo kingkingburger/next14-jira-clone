39d65693368eae45048ef740e3e38d6f6f035849
minho <dnjsalsgh123@gmail.com>
2025-03-11 20:42:22 +0900

    update: create project post api

--- src/features/project/server/route.ts
+++ src/features/project/server/route.ts
@@ -1,43 +1,99 @@
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { z } from "zod";
 
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { getMembers } from "@/features/members/util";
-import { DATABASE_ID, PROJECT_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECT_ID } from "@/config";
+import { createProjectSchema } from "@/features/project/schema";
 
-const app = new Hono().get(
-  "/",
-  sessionMiddleware,
-  zValidator("query", z.object({ workspaceId: z.string() })),
-  async (c) => {
-    const user = c.get("user");
-    const databases = c.get("databases");
+const app = new Hono()
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("form", createProjectSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
 
-    const { workspaceId } = c.req.valid("query");
+      const { name, image, workspaceId } = c.req.valid("form");
 
-    if (!workspaceId) {
-      return c.json({ error: "Missing workspaceId" }, 400);
-    }
+      const member = await getMembers({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
 
-    const member = await getMembers({
-      databases,
-      workspaceId,
-      userId: user.$id,
-    });
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-    if (!member) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
+      let uploadedImageUrl: string | undefined;
 
-    const projects = await databases.listDocuments(DATABASE_ID, PROJECT_ID, [
-      Query.equal("workspaceId", workspaceId),
-      Query.orderDesc("$createdAt"),
-    ]);
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image,
+        );
 
-    return c.json({ data: projects });
-  },
-);
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id,
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+      }
+
+      const project = await databases.createDocument(
+        DATABASE_ID,
+        PROJECT_ID,
+        ID.unique(),
+        {
+          name: name,
+          userId: user.$id,
+          imageUrl: uploadedImageUrl,
+          workspaceId: workspaceId,
+        },
+      );
+
+      return c.json({ data: project });
+    },
+  )
+
+  .get(
+    "/",
+    sessionMiddleware,
+    zValidator("query", z.object({ workspaceId: z.string() })),
+    async (c) => {
+      const user = c.get("user");
+      const databases = c.get("databases");
+
+      const { workspaceId } = c.req.valid("query");
+
+      if (!workspaceId) {
+        return c.json({ error: "Missing workspaceId" }, 400);
+      }
+
+      const member = await getMembers({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const projects = await databases.listDocuments(DATABASE_ID, PROJECT_ID, [
+        Query.equal("workspaceId", workspaceId),
+        Query.orderDesc("$createdAt"),
+      ]);
+
+      return c.json({ data: projects });
+    },
+  );
 
 export default app;
