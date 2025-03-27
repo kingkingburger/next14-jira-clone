8be18853702a4b190acb537dc69b33cf98fa246a
minho <dnjsalsgh123@gmail.com>
2025-03-27 20:06:44 +0900

    update: implement the patch api of project

--- src/features/project/server/route.ts
+++ src/features/project/server/route.ts
@@ -6,7 +6,11 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { getMembers } from "@/features/members/util";
 import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECT_ID } from "@/config";
-import { createProjectSchema } from "@/features/project/schemas";
+import { Project } from "@/features/project/type/types";
+import {
+  createProjectSchema,
+  updateProjectSchema,
+} from "@/features/project/schemas";
 
 const app = new Hono()
   .post(
@@ -93,6 +97,62 @@ const app = new Hono()
 
       return c.json({ data: projects });
     },
-  );
+  )
+  .patch(
+    "/:projectId",
+    sessionMiddleware,
+    zValidator("form", updateProjectSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
 
+      const { projectId } = c.req.param();
+      const { name, image } = c.req.valid("form");
+
+      const existingProject = await databases.getDocument<Project>(
+        DATABASE_ID,
+        PROJECT_ID,
+        projectId,
+      );
+
+      const member = await getMembers({
+        databases,
+        workspaceId: existingProject.workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image,
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id,
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+      } else {
+        uploadedImageUrl = image;
+      }
+
+      const project = await databases.updateDocument(
+        DATABASE_ID,
+        PROJECT_ID,
+        projectId,
+        { name, imageUrl: uploadedImageUrl },
+      );
+
+      return c.json({ data: project });
+    },
+  );
 export default app;
