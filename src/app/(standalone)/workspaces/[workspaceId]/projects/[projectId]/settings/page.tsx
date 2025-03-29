fccb44f773e21b66c0ead8ade5696f095973789d
minho <dnjsalsgh123@gmail.com>
2025-03-30 06:51:11 +0900

    update: initializeValue in project setting component

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -1,4 +1,21 @@
-const ProjectIdSettingPage = () => {
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
+import { getProject } from "@/features/project/queries";
+
+interface ProjectIdSettingPageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+const ProjectIdSettingPage = async ({ params }: ProjectIdSettingPageProps) => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getProject({
+    projectId: params.projectId,
+  });
+
   return <div> ProjectIdSettingPage</div>;
 };
 export default ProjectIdSettingPage;
