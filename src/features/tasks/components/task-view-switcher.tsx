c69f01b610c3d823f72f2a723f75feac09ecf1c9
minho <dnjsalsgh123@gmail.com>
2025-04-11 17:42:21 +0900

    update: apply tabsTrigger in TaskViewSwitcher component

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,3 +1,23 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export const TaskViewSwitcher = () => {
-  return <div>Tasks</div>;
+  return (
+    <Tabs className="flex-1 w-full border rounded-lg">
+      <div className="h-full flex flex-col overflow-auto p-4">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
+              Table
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
+              Kanban
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
+              calendar
+            </TabsTrigger>
+          </TabsList>
+        </div>
+      </div>
+    </Tabs>
+  );
 };
