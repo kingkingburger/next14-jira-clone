211aebc5976b49109694f17e33ead3b5bbbdd58b
minho <dnjsalsgh123@gmail.com>
2025-04-19 05:16:00 +0900

    update: initialize create-task-modal

---
+++ src/features/tasks/components/create-task-modal.tsx
@@ -0,0 +1,12 @@
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
+import { ResponsiveModal } from "@/components/responsive-modal";
+
+export const CreateTaskModal = () => {
+  const { isOpen, setIsOpen } = useCreateTaskModal();
+
+  return (
+    <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
+      <div>TODO: Task form</div>
+    </ResponsiveModal>
+  );
+};
