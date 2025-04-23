ada9660c1fc2914e1dda8ae69c01a3a5e231f14a
minho <dnjsalsgh123@gmail.com>
2025-04-23 20:18:04 +0900

    update: initialize loading card component

--- src/features/tasks/components/create-task-form-wrapper.tsx
+++ src/features/tasks/components/create-task-form-wrapper.tsx
@@ -1,6 +1,9 @@
 import { useGetProjects } from "@/features/project/api/use-get-projects";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useGetMembers } from "@/features/members/api/use-get-members";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+import { Loader } from "lucide-react";
 
 interface CreateTaskFormWrapper {
   onCancel: () => void;
@@ -8,6 +11,33 @@ interface CreateTaskFormWrapper {
 
 export const CreateTaskFormWrapper = ({ onCancel }: CreateTaskFormWrapper) => {
   const workspaceId = useWorkspaceId();
-  const { data: projects, isLoading: isLoadingProjects } = useGetProjects();
-  const { data: members, isLoading: isLoadingMembers } = useGetMembers();
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const projectOptions = projects?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+    imageUrl: project.imageUrl,
+  }));
+
+  const memberOptions = projects?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+  }));
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  if (isLoading) {
+    return (
+      <Card className="w-full h-[714px] border-none shadow-none">
+        <CardContent className="flex items-center justify-center h-full">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </CardContent>
+      </Card>
+    );
+  }
 };
