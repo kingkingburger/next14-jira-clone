323dbb8fbc47f289a2fd041079b278ef1ea51939
minho <dnjsalsgh123@gmail.com>
2025-05-08 20:40:50 +0900

    update: setting option in get task api

--- src/features/tasks/api/use-get-tasks.ts
+++ src/features/tasks/api/use-get-tasks.ts
@@ -1,17 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc";
+import { TaskStatus } from "@/features/tasks/types";
 
 interface UseGetTasksProps {
   workspaceId: string;
+  projectId?: string | null;
+  status?: TaskStatus | null;
+  assigneeId?: string | null;
+  search?: string | null;
+  dueDate?: string | null;
 }
 
-export const useGetTasks = ({ workspaceId }: UseGetTasksProps) => {
+export const useGetTasks = ({
+  workspaceId,
+  projectId,
+  status,
+  assigneeId,
+  search,
+  dueDate,
+}: UseGetTasksProps) => {
   const query = useQuery({
-    queryKey: ["Tasks", workspaceId],
+    queryKey: [
+      "Tasks",
+      workspaceId,
+      projectId,
+      status,
+      assigneeId,
+      search,
+      dueDate,
+    ],
     queryFn: async () => {
       const response = await client.api.tasks.$get({
-        query: { workspaceId },
+        query: {
+          workspaceId,
+          projectId: projectId ?? undefined,
+          status: status ?? undefined,
+          assigneeId: assigneeId ?? undefined,
+          search: search ?? undefined,
+          dueDate: dueDate ?? undefined,
+        },
       });
 
       if (!response.ok) {
