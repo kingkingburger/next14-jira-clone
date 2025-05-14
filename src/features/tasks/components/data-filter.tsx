033577ab426f866a4450f012b2df3e761b35aa1c
minho <dnjsalsgh123@gmail.com>
2025-05-14 19:52:50 +0900

    update: setting filter status in url

--- src/features/tasks/components/data-filter.tsx
+++ src/features/tasks/components/data-filter.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/select";
 import { ListChecksIcon } from "lucide-react";
 import { TaskStatus } from "@/features/tasks/types";
+import { useTaskFilters } from "@/features/tasks/hooks/use-task-filters";
 
 interface DataFiltersProps {
   hideProjectFilter?: boolean;
@@ -38,11 +39,27 @@ export const DataFilters = ({ hideProjectFilter }: DataFiltersProps) => {
     label: member.name,
   }));
 
+  const [{ projectId, status, assigneeId, dueDate }, setFilters] =
+    useTaskFilters();
+
+  const onStatusChange = (value: string) => {
+    if (value === "all") {
+      setFilters({ status: null });
+    } else {
+      setFilters({ status: value as TaskStatus });
+    }
+  };
+
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col lg:flex-row gap-2">
-      <Select defaultValue={undefined} onValueChange={() => {}}>
+      <Select
+        defaultValue={status ?? undefined}
+        onValueChange={(value) => {
+          onStatusChange(value);
+        }}
+      >
         <SelectTrigger className="w-full lg:w-auto h-8">
           <div className="flex items-center pr-2">
             <ListChecksIcon className="size-4 mr-2" />
