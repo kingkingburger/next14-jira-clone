a227620ddc25cefab18e87fb184921480baedaa7
minho <dnjsalsgh123@gmail.com>
2025-05-29 20:44:50 +0900

    update: setting assignee data in table

--- src/features/tasks/components/column.tsx
+++ src/features/tasks/components/column.tsx
@@ -5,6 +5,7 @@ import { Task } from "@/features/tasks/types";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ProjectAvatar } from "@/features/project/components/project-avatar";
+import { MembersAvatar } from "@/features/members/components/members-avatar";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -50,6 +51,34 @@ export const columns: ColumnDef<Task>[] = [
             image={project.imageUrl}
           />
           <p className="line-clamp-1">{project.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "assignee",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Assignee
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const assignee = row.original.assignee;
+
+      return (
+        <div className="flex items-center gap-x-2 text-sm font-medium">
+          <MembersAvatar
+            className="size-6"
+            fallbackClassName="text-xs"
+            name={assignee.name}
+          />
+          <p className="line-clamp-1">{assignee.name}</p>
         </div>
       );
     },
