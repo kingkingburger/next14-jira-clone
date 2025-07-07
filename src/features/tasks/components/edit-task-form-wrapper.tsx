7ea823856c695809fcb2de553cec26200df71a72
minho <dnjsalsgh123@gmail.com>
2025-07-07 20:32:42 +0900

    update: initialize edit task form wrapper

---
+++ src/features/tasks/components/edit-task-form-wrapper.tsx
@@ -0,0 +1,52 @@
+import { useGetProjects } from "@/features/project/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+import { Loader } from "lucide-react";
+
+interface EditTaskFormWrapper {
+  onCancel: () => void;
+  id: string;
+}
+
+export const EditTaskFormWrapper = ({ onCancel , id}: EditTaskFormWrapper) => {
+  const workspaceId = useWorkspaceId();
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const projectOptions = projects?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+    imageUrl: project.imageUrl,
+  }));
+
+  const memberOptions = members?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+  }));
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  if (isLoading) {
+    return (
+      <Card className="w-full h-[714px] border-none shadow-none">
+        <CardContent className="flex items-center justify-center h-full">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    // <EditTaskForm
+    //   onCancel={onCancel}
+    //   projectOptions={projectOptions ?? []}
+    //   memberOptions={memberOptions ?? []}
+    // />
+  );
+};
--- src/features/tasks/components/edit-task-modal.tsx
+++ src/features/tasks/components/edit-task-modal.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { ResponsiveModal } from "@/components/responsive-modal";
-import { CreateTaskFormWrapper } from "@/features/tasks/components/create-task-form-wrapper";
 import { useEditTaskModal } from "@/features/tasks/hooks/use-edit-task-modal";
+import { EditTaskFormWrapper } from "@/features/tasks/components/edit-task-form-wrapper";
 
 export const EditTaskModal = () => {
   const { taskId, close } = useEditTaskModal();
 
   return (
     <ResponsiveModal open={!!taskId} onOpenChange={close}>
-      {taskId && <CreateTaskFormWrapper onCancel={close} />}
+      {taskId && <EditTaskFormWrapper id={taskId} onCancel={close} />}
     </ResponsiveModal>
   );
 };
