88141fc183d6270f5237e685e1a236dfc5e96a30
minho <dnjsalsgh123@gmail.com>
2025-08-02 04:41:08 +0900

    update: setting intialTasks in kanban component

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -1,4 +1,5 @@
 import { Task, TaskStatus } from "@/features/tasks/types";
+import { useState } from "react";
 
 const boards: TaskStatus[] = [
   TaskStatus.BACKLOG,
@@ -19,5 +20,25 @@ interface DataKanbanProps {
 export const DataKanban: React.FC<DataKanbanProps> = ({
   data,
 }: DataKanbanProps) => {
+  const [tasks, setTasks] = useState<TaskStatus>(() => {
+    const initialTasks: TasksState = {
+      [TaskStatus.BACKLOG]: [],
+      [TaskStatus.TODO]: [],
+      [TaskStatus.IN_PROGRESS]: [],
+      [TaskStatus.IN_PREVIEW]: [],
+      [TaskStatus.DONE]: [],
+    };
+    data.forEach((task: Task) => {
+      initialTasks[task.status].push(task);
+    });
+
+    Object.keys(initialTasks).forEach((status) => {
+      initialTasks[status as TaskStatus].sort(
+        (a, b) => a.position - b.position,
+      );
+    });
+
+    return initialTasks;
+  });
   return <div>Data Kanban</div>;
 };
