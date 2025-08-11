3371e4990efa9e9d744e5b0460605e8253a08d77
minho <dnjsalsgh123@gmail.com>
2025-08-11 20:22:39 +0900

    update: adding point in taskCount

--- src/features/tasks/components/kanban-column-header.tsx
+++ src/features/tasks/components/kanban-column-header.tsx
@@ -39,8 +39,10 @@ export const KanbanColumnHeader = ({
     <div className="px-2 py-1.5 flex items-center justify-between">
       <div className="flex items-center gap-x-2">
         {icon}
-        <h2>{snakeCaseToTitleCase(board)}</h2>
-        <div>{taskCount}</div>
+        <h2 className="text-sm font-medium">{snakeCaseToTitleCase(board)}</h2>
+        <div className="size-5 flex items-center justify-center rounded-md bg-neutral-200 text-xs text-neutral-700 font-medium">
+          {taskCount}
+        </div>
       </div>
     </div>
   );
